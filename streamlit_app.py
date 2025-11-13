4b7790154dbce7f9904f1f741afad05e8e85a761
nguyenvande06 <nguyenvande466@gmail.com>
2025-11-13 21:34:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,8 +106,7 @@ default_edges_text = """
 1 5
 4 5
 5 6
-7 # Đỉnh cô lập
-"""
+
 # ------------------------------
 # HÀM VẼ CHUNG (Đã tích hợp st.status)
 # ------------------------------
@@ -450,7 +449,7 @@ if st.session_state.G and len(st.session_state.G.nodes()) > 0:
         st.session_state.pos,
         st.session_state.nodes_list,
         st.session_state.color_assign,
-        title="Đồ thị đã tải (Chưa tô màu)",
+        title="",
         delay=0.0
     )
 else:
